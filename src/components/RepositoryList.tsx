7de3a4eb35f1acc26723aeaaa11ad3a651a04d8c
iury.franca <iurygfranca@gmail.com>
2021-09-24 12:18:26 -0300

    feat: usando typescript nas varipaveis do projeto ✨

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,17 +3,20 @@ import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
 
-export function RepositoryList () {
-  const [repositories, setRepositories] = useState([])
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
+export function RepositoryList () {
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
       .then(response => response.json())
       .then(data => setRepositories(data))
   }, []);
-
-  console.log(repositories)
 
   return (
     <section className="repository-list">
